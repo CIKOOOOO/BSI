052baac29cd5d29c048fd47981815ff5a30ccde9
Gayung <clarissa.aristania001@gmail.com>
2020-12-21 08:50:28 +0700

    Tambah base project di awal spt utils + constant + values

---
+++ app/src/main/java/com/bca/bsi/utils/Constant.java
@@ -0,0 +1,4 @@
+package com.bca.bsi.utils;
+
+public class Constant {
+}
---
+++ app/src/main/java/com/bca/bsi/utils/Utils.java
@@ -0,0 +1,65 @@
+package com.bca.bsi.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Utils {
+
+    public static void hideSoftKeyboard(Activity activity) {
+        if (activity == null) return;
+        else if (activity.getCurrentFocus() == null) return;
+        InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+    }
+
+    public static void setupUI(View view, final Activity activity) {
+        // Set up touch listener for non-text box views to hide keyboard.
+        if (!(view instanceof EditText)) {
+            view.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View v, MotionEvent event) {
+                    hideSoftKeyboard(activity);
+                    return false;
+                }
+            });
+        }
+
+        //If a layout container, iterate over children and seed recursion.
+        if (view instanceof ViewGroup) {
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+                View innerView = ((ViewGroup) view).getChildAt(i);
+                setupUI(innerView, activity);
+            }
+        }
+    }
+
+    public static String priceFormat(long totalPrice) {
+        DecimalFormat formatter = new DecimalFormat("#,###,###");
+        return formatter.format(totalPrice);
+    }
+
+    public static String getTime(String format) {
+        SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.getDefault());
+        return sdf.format(new Date());
+    }
+
+    public static String formatDateFromDateString(String inputDateFormat, String outputDateFormat, String inputDate) throws ParseException {
+        Date mParsedDate;
+        String mOutputDateString;
+        SimpleDateFormat mInputDateFormat = new SimpleDateFormat(inputDateFormat, java.util.Locale.getDefault());
+        SimpleDateFormat mOutputDateFormat = new SimpleDateFormat(outputDateFormat, java.util.Locale.getDefault());
+        mParsedDate = mInputDateFormat.parse(inputDate);
+        mOutputDateString = mOutputDateFormat.format(mParsedDate);
+        return mOutputDateString;
+    }
+}
