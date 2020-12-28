f4654aab1fa13b123134a1ed9f2503d1bea73524
Dewi Salma Salsabila <dewisalmasalsabila@gmail.com>
2020-12-28 14:15:49 +0700

    Kalkulator

--- app/src/main/java/com/bca/bsi/ui/basenavigation/more/CalculatorMore/BesarHasilInvestasiFragment.java
+++ app/src/main/java/com/bca/bsi/ui/basenavigation/more/CalculatorMore/BesarHasilInvestasiFragment.java
@@ -2,15 +2,25 @@ package com.bca.bsi.ui.basenavigation.more.CalculatorMore;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.bca.bsi.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BesarHasilInvestasiFragment extends Fragment {
+
+    private Spinner spinnerDurasiTahunBHI;
+    private Spinner spinnerDurasiBulanBHI;
 
     public BesarHasilInvestasiFragment() {
         // Required empty public constructor
@@ -22,5 +32,30 @@ public class BesarHasilInvestasiFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_besar_hasil_investasi, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        List<Integer> durasiTahun = new ArrayList<Integer>();
+        List<Integer> durasiBulan = new ArrayList<Integer>();
+
+        for (int i = 0; i < 51; i++) {
+            durasiTahun.add(i);
+        }
+
+        for (int j = 0; j < 12; j++) {
+            durasiBulan.add(j);
+        }
+
+        spinnerDurasiTahunBHI = view.findViewById(R.id.bhi_durasi_tahun);
+        spinnerDurasiBulanBHI = view.findViewById(R.id.bhi_durasi_bulan);
+
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(view.getContext(), android.R.layout.simple_dropdown_item_1line, durasiTahun);
+        spinnerDurasiTahunBHI.setAdapter(adapter);
+
+        ArrayAdapter<Integer> adapterBulan = new ArrayAdapter<Integer>(view.getContext(), android.R.layout.simple_dropdown_item_1line, durasiBulan);
+        spinnerDurasiBulanBHI.setAdapter(adapterBulan);
     }
 }
