4975daadf9b8139f61a0fcfe3bb57bf47739ac63
CIKOOOOO <abedcreen@gmail.com>
2020-12-30 17:45:08 +0700

    Tambah fragment information promo

---
+++ app/src/main/java/com/bca/bsi/ui/basenavigation/information/PromoNewsAdapter.java
@@ -0,0 +1,66 @@
+package com.bca.bsi.ui.basenavigation.information;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bca.bsi.R;
+import com.bca.bsi.model.PromoNews;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PromoNewsAdapter extends RecyclerView.Adapter<PromoNewsAdapter.Holder> {
+
+    private List<PromoNews> promoNewsList;
+    private onItemClick onItemClick;
+
+    public interface onItemClick {
+        void onObjectClick(PromoNews promoNews);
+    }
+
+    public PromoNewsAdapter(PromoNewsAdapter.onItemClick onItemClick) {
+        this.onItemClick = onItemClick;
+        this.promoNewsList = new ArrayList<>();
+    }
+
+    public void setPromoNewsList(List<PromoNews> promoNewsList) {
+        this.promoNewsList = promoNewsList;
+    }
+
+    @NonNull
+    @Override
+    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_promo_news, parent, false);
+        return new Holder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Holder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return promoNewsList.size();
+    }
+
+    static class Holder extends RecyclerView.ViewHolder {
+
+        private TextView tvTitle, tvDesc, tvDate;
+        private ImageView imgView;
+
+        public Holder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.recycler_tv_title_promo_news);
+            tvDesc = itemView.findViewById(R.id.recycler_tv_description_promo_news);
+            tvDate = itemView.findViewById(R.id.recycler_tv_date_promo_news);
+            imgView = itemView.findViewById(R.id.recycler_img_promo_news);
+        }
+    }
+}
