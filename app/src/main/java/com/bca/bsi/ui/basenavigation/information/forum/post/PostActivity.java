b1b902685ce2e4bcc4fbdc76cfd2696f3c429dbb
Gayung <clarissa.aristania001@gmail.com>
2021-02-24 14:22:11 +0700

    Update PostActivity.java

--- app/src/main/java/com/bca/bsi/ui/basenavigation/information/forum/post/PostActivity.java
+++ app/src/main/java/com/bca/bsi/ui/basenavigation/information/forum/post/PostActivity.java
@@ -203,7 +203,7 @@ public class PostActivity extends BaseActivity implements PrivacyAdapter.onPriva
                             }
 
                             //TODO: check disini share trade
-                            double price = ((this.information.getCost() / this.information.getNab()) - 1);
+                            double price = ((this.information.getNab() * this.information.getUnit()) - this.information.getCost()) / this.information.getCost();
 
                             tvTransactionType.setText(value);
                             tvNameShareTrade.setText(this.information.getName());
